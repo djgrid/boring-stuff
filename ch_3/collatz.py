910e1dbbb441df5985e14b4475b780495a86c58f
= <=>
2019-12-31 15:19:14 -0500

    Pretty satisfied with this version.

--- ch_3/collatz.py
+++ ch_3/collatz.py
@@ -1,18 +1,37 @@
 # From Chapter 3 of Automate the Boring Stuff.
+#
+# This program implements the Collatz sequence. 
+#
 
+# The book wanted me to print from the function but that 
+# did not sit well with me so instead I just return the
+# value and print it in the main program.
+#
+# If my knowledge wasn't so rusty I could probably make this
+# a recursive function and just call it once from within main.
 def collatz(number):
-	if number % 2 == 0:
-		return number // 2
-	else:
-		return 3 * number + 1
+    if number % 2 == 0:
+        return number // 2
+    else:
+        return 3 * number + 1
 
-print("Enter an integer")
-userint = int(input())
+# Using exception handling for input validation seems
+# kinda clunky but I can't think of another way to do it
+# 
+# There is probably a better way to do this.
+userint = " "
+while not isinstance(userint, int):
+    print("Enter an integer")
+    try:
+        userint = int(input())
+    except ValueError:
+        print("No, an integer. Want to try again?")
+
 
 result = collatz(userint)
 print(result)
 
 while result != 1:
-	result = collatz(result)
-	print(result)
+    result = collatz(result)
+    print(result)
 
