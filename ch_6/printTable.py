7bd601c6f5c29fff1061d2a96d722f88052df488
= <=>
2020-01-17 12:55:34 -0500

    Added more tables to verify program works

--- ch_6/printTable.py
+++ ch_6/printTable.py
@@ -8,7 +8,7 @@ def printTable(items):
     for i in range(len(items)):
         colWidths.append(len(max(items[i], key=len)))
 
-    just = max(colWidths)
+    just = max(colWidths) + 1
 
     for i in range(len(items[0])):
         for j in range(len(items)):
@@ -21,5 +21,34 @@ def printTable(items):
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
+
+printTable(tableData)
+
+print("")
+print("---------")
+print("")
+
+tableData = [['John', 'Paul', 'George', 'Ringo'],
+             ['Pete', 'Roger', 'John', 'Keith'],
+             ['Gordon', 'Brian', 'Blaise', 'John'],
+             ['Naoko', 'Ritsuko', 'Atsuko', 'Risa'],
+             ['Michael', 'Peter', 'Mike', 'Bill']]
+
+printTable(tableData)
+
+print("")
+print("---------")
+print("")
+
+# This one has more than 80 cols but works if you make the 
+# terminal window wider.
+tableData = [['Osaka', 'Kobe', 'Sapporo'],
+             ['Boston', 'Washington', 'Winnemucca'],
+             ['London', 'Leicestershire', 'Bristol'],
+             ['Paris', 'Nice', 'Le Havre'],
+             ['Ottawa', 'Kitchener', 'Vancouver'],
+             ['Dusseldorf', 'Frankfurt', 'Berlin'],
+             ['Glasgow', 'Edinburgh', 'Inverness'],
+             ['Melbourne', 'Lightning Ridge', 'Alice Springs']]
 
 printTable(tableData)
